f000886d2c8dade8f80a26409f51ac5346977eaa
Midex <olamideolorunshola19@gmail.com>
2026-06-01 04:39:39 +0100

    Vercl

--- Frontend/hospital-frontend/src/api/api.js
+++ Frontend/hospital-frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://publicly-documented-ranger-shadows.trycloudflare.com/api",
+  baseURL:
+    "https://assembled-preceding-referring-editorial.trycloudflare.com/api",
 });
 
 api.interceptors.request.use((config) => {
